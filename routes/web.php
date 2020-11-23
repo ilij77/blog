84a2f3455fc9db9a1000e716be121eed78df5ce6
Ilij <ilij@ya.ru>
2020-11-24 01:25:39 +0300

    Урок13

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::group(['prefix'=>'admin','namespace'=>'admin'],function (){
+Route::group(['prefix'=>'admin','namespace'=>'admin','middleware'=>'admin'],function (){
     Route::get('/','MainController@index')->name('admin.index');
     Route::resource('/categories','CategoryController');
     Route::resource('/tags','TagController');
     Route::resource('/posts','PostController');
 });
-Route::get('/register','UserController@create')->name('register.create');
-Route::post('/register','UserController@store')->name('register.store');
+
+Route::group(['middleware'=>'guest'],function (){
+    Route::get('/register','UserController@create')->name('register.create');
+    Route::post('/register','UserController@store')->name('register.store');
+    Route::get('/login','UserController@loginForm')->name('login.create');
+    Route::post('/login','UserController@login')->name('login');
+});
+
+
+Route::get('/logout','UserController@logout')->name('logout')->middleware('auth');
